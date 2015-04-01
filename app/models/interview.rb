f6ce21ef513a45b4b5e32f995f43920dd0ac1aaa
alex miley <alex.miley@gmail.com>
2015-03-31 21:19:05 -0700

    add easier way to add interview

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,8 @@
 class Interview < ActiveRecord::Base
   belongs_to :position
   has_one :company, through: :position
+
+  def position_at_company
+    "#{position.title} at #{position.company.name}"
+  end
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,8 @@
 class Position < ActiveRecord::Base
   belongs_to :company
   has_many :interviews, dependent: :destroy
+
+  def position_at_company
+    "#{title} at #{company.name}"
+  end
 end
